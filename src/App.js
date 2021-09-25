2f4f0455e81ee1d8b343ed7822871e9987b1af60
netstermonir <monirhossain6163@gmail.com>
2021-09-26 02:07:25 +0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState([]);
+  useEffect(() => {
+    fetch('./fake-dev.JSON')
+      .then(res => res.json())
+      .then(data => {
+        setImage(data);
+        console.log(data)
+      })
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        image.map(imgs =>
+          <>
+            <h2>{imgs.name}</h2>
+            <img src={imgs.images} alt="" />
+          </>
+        )
+      }
     </div>
   );
 }
