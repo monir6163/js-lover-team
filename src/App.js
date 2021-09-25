223ac638cff1b4e32aec29732e74112ef43eb9ba
netstermonir <monirhossain6163@gmail.com>
2021-09-26 04:12:13 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import Banner from './Components/Banner/Banner';
+import Header from './Components/Header/Header';
 
 function App() {
-  const [image, setImage] = useState([]);
-  useEffect(() => {
-    fetch('./fake-dev.JSON')
-      .then(res => res.json())
-      .then(data => {
-        setImage(data);
-        console.log(data)
-      })
-  }, []);
   return (
-    <div className="App">
-      {
-        image.map(imgs =>
-          <>
-            <h2>{imgs.name}</h2>
-            <img src={imgs.images} alt="" />
-          </>
-        )
-      }
+    <div>
+      <Header></Header>
+      <Banner></Banner>
     </div>
   );
 }
